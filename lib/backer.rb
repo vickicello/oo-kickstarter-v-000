76e63bf538d0712ddedeabcc04fbf90061bc407b
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 18:27:22 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,24 @@
+class Backer
+
+  attr_accessor :name, :backed_projects
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+    @@all << @name
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+end
