b145998e2ce8ac1b748b2c03accf1f272284de89
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 18:22:55 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,16 @@
 class Backer
 
   attr_accessor :name, :backed_projects
+  @@all = []
 
   def initialize(name)
     @name = name
     @backed_projects = []
+    @@all < @name
   end
 
-  def back_project(project, backer)
-    @backed_projects << (project, backer)
+  def back_project(project)
+    @backed_projects << project, backer
   end
 
 
